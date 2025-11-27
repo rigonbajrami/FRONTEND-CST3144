25596886ccb1966e9756a00f46937dbd0d752c09
rigonbajrami <rigon04@outlook.com>
2025-11-27 22:28:18 +0000

    Feat: Add GitHub Pages deployment workflow

--- src/router/index.js
+++ src/router/index.js
@@ -1,11 +1,11 @@
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHashHistory } from 'vue-router';
 import LessonsView from '../views/LessonsView.vue';
 import CartView from '../views/CartView.vue';
 import LoginView from '../views/LoginView.vue';
 import RegisterView from '../views/RegisterView.vue';
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(),
   routes: [
     { path: '/', name: 'lessons', component: LessonsView },
     { path: '/cart', name: 'cart', component: CartView },
--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,8 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/FRONTEND-CST3144/',
   plugins: [
     vue(),
     vueJsx(),
